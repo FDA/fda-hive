00f4cf346c80cd34a7538fe9816bc34f9cfc1d3b
Anton Golikov <anton.golikow@gmail.com>
2026-09-11 14:30:12 -0400

    revision 3

--- hive/project/hive-assembly/hive-assembly.cpp
+++ hive/project/hive-assembly/hive-assembly.cpp
@@ -46,6 +46,11 @@ CFLOW_START(HIVEASSEMBLY,"hive-assembly")
     sStrT name;
     formValue("name",&name);
 
+    sStr hoxID;
+    cfgStr(&hoxID, 0, "HIVE.hoxID", "-");
+    hoxID.printf("_%s", reportO->IdStr());
+    reportO->propSet("accession", hoxID.ptr());
+
     if (!name.length()) {
         name.printf("hive-assembly_%s",objs[0].IdStr());
         pForm->inp("name",name.ptr());
@@ -89,7 +94,7 @@ CFLOW_START(HIVEASSEMBLY,"hive-assembly")
                 continue;
             }
             hexagonName.printf(0,"p_alignment-sample.%s",ref);
-            buf.printf(0,"Reads sample vs %s_%s",hqry.propGet("name"),ref);
+            buf.printf(0,"%s_Reads sample vs %s_%s",name.ptr(),hqry.propGet("name"),ref);
             varset.inp("hexName",buf.ptr());
             varset.inp("_o_reference_genome",ref);
             ++alTotal;
@@ -150,7 +155,7 @@ CFLOW_START(HIVEASSEMBLY,"hive-assembly")
         }
         cloneDeflineFastaTmplt.printf(0,"$_(v) reference:%.*s clone_cov:%" DEC " source:hive-assembly", (int)refLen, refName, clone_cov);
         clonalName.printf(0,"p_clonal-analysis.%s",ref);
-        buf.printf(0,"%s_%s",hqry.propGet("name"),ref);
+        buf.printf(0,"%s_%s_%s",name.ptr(),hqry.propGet("name"),ref);
         varset.inp("clonalName",buf.ptr());
         
         buf.printf(0,"p_alignment-sample.%s",ref);
@@ -167,8 +172,21 @@ CFLOW_START(HIVEASSEMBLY,"hive-assembly")
         if(!ensureProcess(clonalName.ptr()))
             continue;
 
+        idx cloneCount = getCloneCount(user, procId(clonalName.ptr()));
+
+        if( cloneCount == sNotIdx || cloneCount == 0 ) {
+            logOut(
+                cloneCount == 0 ? eQPLogType_Info : eQPLogType_Warning,
+                cloneCount == 0
+                    ? "No clones found for reference %s; skipping remaining steps"
+                    : "Could not read clones for reference %s; skipping remaining steps",
+                ref
+            );
+            ++procCounter;
+            continue;
+        }
         buf.printf(0,"p_archiver_clones.%s",ref);
-        buf2.printf(0,"%s_clones.fasta",hqry.propGet("name"));
+        buf2.printf(0,"%s_%s_clones.fasta",name.ptr(),hqry.propGet("name"));
         idx res_ClonalContigs = archiveCGI(buf.ptr(),clonalName.ptr(), buf2.ptr(),"cmd=popExtended&fasta_tmplt=%.*s&noGapsFrame=0&contig_print=seq&minFrequency=0.5&minDiversity=1&simThrs=50&maskLowDiversity=1&covThrs=%" DEC "&minCloneLen=%" DEC "&mergeHidden=1&minCloneCov=%" DEC "",(int)cloneDeflineFastaTmplt.length(),cloneDeflineFastaTmplt.ptr(),clone_cov,clone_len,clone_support);
         if(res_ClonalContigs) {
             buf2.printf(0,"o_clones.%s",ref);
@@ -178,7 +196,7 @@ CFLOW_START(HIVEASSEMBLY,"hive-assembly")
             continue;
         
         procName.printf(0,"p_alignment-sample-clonal.%s",ref);
-        buf.printf(0,"%s_%s-vs_clones",hqry.propGet("name"),ref);
+        buf.printf(0,"%s_%s_%s-vs_clones",name.ptr(),hqry.propGet("name"),ref);
         varset.inp("hexClonalName",buf.ptr());
 
         buf.printf(0,"o_clones.%s",ref);
@@ -190,7 +208,7 @@ CFLOW_START(HIVEASSEMBLY,"hive-assembly")
             continue;
 
         procName.printf(0,"p_hepta-profiler.%s",ref);
-        buf.printf(0,"%s_%s-vs_clones",hqry.propGet("name"),ref);
+        buf.printf(0,"%s_%s_%s-vs_clones",name.ptr(),hqry.propGet("name"),ref);
         varset.inp("heptClonalName",buf.ptr());
 
         buf.printf(0,"p_alignment-sample-clonal.%s",ref);
@@ -202,16 +220,84 @@ CFLOW_START(HIVEASSEMBLY,"hive-assembly")
             continue;
 
         buf.printf(0,"p_archiver_consensus.%s",ref);
-        buf2.printf(0,"%.*s_clones_consensus.fasta",(int)refLen,refName);
+        buf2.printf(0,"%s_%.*s_clones_consensus.fasta",name.ptr(),(int)refLen,refName);
         idx res_ClonalConsensus = archiveCGI(buf.ptr(),procName.ptr(), buf2.ptr(),"cmd=profConsensus&idSub=0&gaps=skip&arch=1&backend=1&check=1&down=0&raw=0");
         if(res_ClonalConsensus) {
             buf2.printf(0,"o_clones_consensus.%s",ref);
             res_ClonalConsensus=searchArchived(buf2.ptr(),buf.ptr(),"nuc-read");
-            if (!res_ClonalConsensus) {
-                res_ClonalConsensus=searchArchived(buf2.ptr(),buf.ptr(),"genome");
-            }
+            if (res_ClonalConsensus) {
+                buf.printf(0,"%" DEC "",procId(buf2.ptr()));
+                buf2.printf(0,"%s/%s_%.*s_clones_consensus-dedup.fasta",procFolder.ptr(),name.ptr(),(int)refLen,refName);
+
+                if( !sFile::exists(buf2.ptr())){ 
+                    sviolin::sHiveseq sf(user, buf.ptr(),sviolin::sHiveseq::eBioModeShort);
+                    sFil myFile(buf2.ptr(), sMex::fMapRemoveFile);
+                    sf.printFastX(&myFile, false,0,sf.dim(),0,true);
+                    myFile.destroy();
+                }
+                
+                buf.printf(0,"file://%s",buf2.ptr());        
+                buf2.printf(0,"%s_%.*s_clones_consensus-dedup.fasta",name.ptr(),(int)refLen,refName);
+                procName.printf(0, "p_uploader_dedup.%s", ref);
+                res_ClonalConsensus=ensureDownload(procName.ptr(),buf2.ptr(),"0",buf.ptr());
+                if (res_ClonalConsensus) {
+                    buf.printf(0,"o_clones_dedup.%s",ref);
+                    res_ClonalConsensus=searchArchived(buf.ptr(),procName.ptr(),"nuc-read");
+                }
+            }    
+            
         }
         if(!res_ClonalConsensus)
+            continue;
+
+        procName.printf(0,"p_alignment-clonal-ref-id.%s",ref);
+        buf.printf(0,"%s_%s_%s-vs_clones_consensus_dedup",name.ptr(),hqry.propGet("name"),ref);
+        varset.inp("hexClonalRefIDName",buf.ptr());
+
+        buf.printf(0,"o_clones_dedup.%s",ref);
+        idx cloneId = procId(buf.ptr());
+        buf.printf(0,"%" DEC "",cloneId);
+        varset.inp("_o_clones_dedup",buf.ptr());
+        varset.inp("_o_reference_genome_dedup",ref);
+
+        if(!ensureProcess(procName.ptr()))
+            continue;
+
+        buf.printf(0,"p_archiver_consensus_id_table.%s",ref);
+        buf2.printf(0,"%s_%.*s_clones_consensus_id_hitTable.csv",name.ptr(),(int)refLen,refName);
+        idx res_ClonalConsensusID = archiveCGI(buf.ptr(),procName.ptr(), buf2.ptr(),"cmd=alMatch&info=1&found=1&qty=-1");
+
+        if (res_ClonalConsensusID) {
+            buf2.printf(0,"o_clones_consensus_id_table.%s",ref);
+            res_ClonalConsensusID=searchArchived(buf2.ptr(),buf.ptr(),"csv-table");
+            if (res_ClonalConsensusID) {
+                sStr hitTablePath;
+                getFilePath(buf2.ptr(),"_.csv",0,&hitTablePath);
+                buf2.printf(0,"%s_%.*s_clones_consensus_dedup_id_final.fasta",name.ptr(),(int)refLen,refName);
+                CloneInfo curClones;
+                curClones.objId = cloneId; curClones.procId = reportObjId;
+                curClones.procName = name.ptr();
+                curClones.procFolder = procFolder.ptr();
+                curClones.hoxID = hoxID.ptr();
+                fixIdLineClones(hitTablePath.ptr(), curClones, user, buf2.ptr(),buf);
+                buf2.printf(0,"file://%s",buf.ptr());
+                buf.printf(0,"%s_%.*s_clones_consensus_dedup_id_final.fasta",name.ptr(),(int)refLen,refName);
+                procName.printf(0, "p_uploader_final.%s", ref);
+                res_ClonalConsensusID=ensureDownload(procName.ptr(),buf.ptr(),"0",buf2.ptr());
+                if (res_ClonalConsensusID) {
+                    buf.printf(0,"o_clones_final.%s",ref);
+                    res_ClonalConsensusID=searchArchived(buf.ptr(),procName.ptr(),"nuc-read");
+                    if (!res_ClonalConsensusID) {
+                        res_ClonalConsensusID=searchArchived(buf.ptr(),procName.ptr(),"genome");
+                    }
+                }
+
+            }
+
+        }
+
+
+        if(!res_ClonalConsensusID)
             continue;
 
         ++procCounter;
@@ -224,7 +310,7 @@ CFLOW_START(HIVEASSEMBLY,"hive-assembly")
     setEpoch("Final");
 
     sStr consensusObjIds; 
-    sString::searchAndReplaceSymbols(&consensusObjIds,listObjRegex(buf.printf(0,"o_clones_consensus[.]*")),0,",",0,0,true,true,true,true,0);
+    sString::searchAndReplaceSymbols(&consensusObjIds,listObjRegex(buf.printf(0,"o_clones_final[.]*")),0,",",0,0,true,true,true,true,0);
 
     for (const char * r=consensusObjIds.ptr(0); r; r=sString::next00(r)) {
         const char * cDone=varset.value(r);
@@ -239,6 +325,8 @@ CFLOW_START(HIVEASSEMBLY,"hive-assembly")
             
             varset.inp(r,"done");
         }
+
+        
     }
 
     reqProgress(100,100, 100);
