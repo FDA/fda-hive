52c1e9fb0e9f13ea37a45d3219d72dbbffdb71d1
Anton Golikov <anton.golikow@gmail.com>
2026-09-11 14:22:18 -0400

    revision 3

--- hive/project/argos/hive-ncbi.hpp
+++ hive/project/argos/hive-ncbi.hpp
@@ -41,9 +41,9 @@ class sHIVENCBI {
     idx sleepSec,sleepStep,sleepGranules;
     sHIVENCBI () {
         debug=0;
-        sleepSec=1;
-        sleepGranules=3;
-        sleepStep=0;
+        sleepSec=3;
+        sleepGranules=2;
+        sleepStep=1;
     }
     const char * eSearch(const char * db, const char * term , sStr * dst=0);
     const char * eFetch(const char * db, const char * id, const char * mode=0, sStr * dst=0);
@@ -54,7 +54,8 @@ class sHIVENCBI {
     
     const char * biosample(const char * acc, sStr * dst=0, sJson * json=0);
 
-    const char * assm2biosample(const char * assmACC, sStr * dst, const char * json);
+    const char * assm2biosample(const char * assmACC, sStr * dst, const char * json, sStr * seqLen);
+    const char * getRefLen(const char * ref, sStr * dst);
     const char * biosample2SRA(const char * biosample, sStr * dst, const char * json);
     const char * biosample2assm(const char * bsAcc, sStr * dst, const char * json);
     const char * assm2genome(const char * assembly, sStr * dst, sStr * outRefseqAssembly);
@@ -66,13 +67,32 @@ class sHIVENCBI {
     const char * parseJson(const char * json, sStr * dist=0);
     const char * srs2srr(const char * srsId, sStr * dst=0);
     bool getLineage(const char *taxID, sStr *strLineage=0, sStr *dst=0);
-    bool flattenNcbiJson(const char * ncbiRawJson, sJson * strucJson, sStr * outJsonStr = 0, sStr * log = 0);
+    bool flattenNcbiJson(const char * ncbiRawJson, sJson * strucJson, sStr * outJsonStr = 0, sStr * log = 0, const char * argos_ID=0);
     bool getUniProtData(const char *speciesName, sJson *strucJson);
     const char * searchUniProt(const char *speciesName, sStr * dst=0);
     const char * getUniProtProteomeID(const char *uniprotJson, sStr *buf=0, const char *speciesName=0);
     const char * mapUniProtID(const char *proteomeID, sStr * dst=0);
     const char * proteinInfoResource(const char *speciesName, sStr *dst=0);
+    const char * assm2sra(const char * assmACC, sStr * dst=0, const char * json=0);
+    const char * srr2biosampleAcc(const char * srr, sStr * dst=0);
+    const char * eSummary(const char * db, const char * id, sStr * dst=0);
+    const char * biosampleUid2Acc(const char * biosample_uid, sStr * dst=0);
+    idx extractBioSampleAcc(const char * txt, sStr & out);
+    const char * eSummaryXML(const char * db, const char * id, sStr * dst=0);
+    const char * srr2biosampleAccXML(const char * srr, sStr * dst=0);
+    const char * getBiosampleMeta(const char * bsAcc, sStr * dst=0, const char * json=0);
+    const char * fetchGenbankData(const char * gbAcc, sStr * dst=0);
+    const char * eFetchRunInfo(const char * sra_uids, sStr * dst=0);
+    const char * sraUid2SRR(const char * sra_uids, sStr * dst=0);
+    const char * biosampleUid2SRR(const char * biosample_uids, sStr * dst=0);
+    const char * assembly2SRR(const char * assembly, sStr * dst=0);
+    const char * nucUid2SRRFromDBLink(const char * nuc_uid, sStr * dst=0);
+    const char * nucUid2SRRThroughBioSample(const char * nuc_uid, sStr * dst=0);
+    const char * nucAcc2CurrentAcc(const char * nuc_acc, sStr * dst=0);
+    const char * nucseq2BiosampleAcc(const char * nuc_acc, sStr * dst=0);
+    const char * nucseq2SRR(const char * nuc_acc, sStr * dst=0);
+    const char * nucseq2BiosampleAccOnce(const char * nuc_acc, sStr * dst=0);
+    const char * nucseq2SRROnce(const char * nuc_acc, sStr * dst=0);
+    const char * getLibaryInfo(const char *srr, sStr *instrumentModel, sStr *libStrategy, sStr *libSource, sStr *libSelection, sStr *libConstProtocol);
 };
-
-
 
